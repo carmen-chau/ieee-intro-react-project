4efd10bb997a3797af840b2f8cc2939dda351802
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2022-12-31 11:58:12 -0500

    added additional comments for Message.js

--- src/Message.js
+++ src/Message.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 const baseURL = "https://dry-shelf-88643.herokuapp.com/https://cloudsking.com/react_api.php"
 
 export default function Message() {
-    //Initializing variables in state
+    //Initializing variables in state using the useState() react hook
     const [formValue, setFormValue] = useState({
         name: "",
         phone_number: "",
@@ -15,19 +15,14 @@ export default function Message() {
 
     //Function to handle changes to variables in state
     function handleChange(event) {
-        const name = event.target.name
-        const value = event.target.value
-        setFormValue((priorState) => {
-            return {
-                ...priorState,
-                [name]: value,
-            };
-        });
-    };
+        const name = event.target.name // getting the name (declared in HTML) of the element that triggered the event
+        const value = event.target.value // getting the value of the element that triggered the event
+        setFormValue({...formValue, [name]: value}) //updating the state variable that was changed, leaving all other variables the same
+    }
 
     //Function to handle form submission
     function handleSubmit(event) {
-        event.preventDefault()
+        event.preventDefault() // preventing default behaviour for form submission
         //Passing in form data (using FormData() object from JS)
         const formData = new FormData()
         formData.append("name", formValue.name)
@@ -35,10 +30,8 @@ export default function Message() {
         formData.append("message", formValue.message)
 
         axios.post(baseURL, formData).then((res) => {
-            //console.log(formData.get("message")) //Displaying (via the console) the message that the user inputs
-            //console.log(JSON.stringify(res.data))
             let formMessage = "Form submission Message: " + JSON.stringify(res.data)
-            setFormValue({...formValue, formSubmissionMessage: formMessage})
+            setFormValue({...formValue, formSubmissionMessage: formMessage}) //updating state variable containing the form submission variable, all other variables remain the same
         })
     }
 
